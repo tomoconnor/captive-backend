f4948b6507527d9a19b3ccd9dad5363a2b3febcd
Tom O'Connor <tom@twinhelix.org>
2012-11-04 09:37:31 +0000

    adding a cleanpath config script, internet, no captive portal

---
+++ bin/start_cleanpath.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+sudo iptables -t nat --flush
+sudo iptables --table nat --append POSTROUTING --out-interface eth0 -j MASQUERADE
+sudo iptables --append FORWARD --in-interface eth1 -j ACCEPT
